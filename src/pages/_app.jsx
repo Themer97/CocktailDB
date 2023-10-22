6740242d87dd37697b2f36335eb2669862a33479
Themer97 <themersaanouni@gmail.com>
2023-10-22 16:11:37 +0100

    made some changes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
-import store from '@/redux/store';
+import { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
-import Navbar from '@/components/layout/Navbar';
-import '../app/globals.css';
+import Navbar from "@/components/layout/Navbar";
+import "../app/globals.css";
 
 function App({ Component, pageProps: { pageProps } }) {
-  const [themeMode, setThemeMode] = useState('light');
+  const [themeMode, setThemeMode] = useState("light");
 
   const handleThemeChange = (newMode) => {
     setThemeMode(newMode);
-    localStorage.setItem('theme', newMode);
+    localStorage.setItem("theme", newMode);
   };
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
+    const storedTheme = localStorage.getItem("theme");
     setThemeMode(storedTheme);
   }, []);
 
